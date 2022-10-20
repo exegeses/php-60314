8facf2b5b65839e7fa5f159cc6c6e9ffc84e3525
Marcos <exegeses@gmail.com>
2022-10-20 11:52:24 -0300

    práctica fuunción fecha + switch()

--- clase2/fecha.php
+++ clase2/fecha.php
@@ -1,1 +1,41 @@
 <?php
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
+    //mostrar 20/10/2022
+    $diaMes = date('d');
+    $numeroMes = date( 'm');
+    $anio = date('Y');
+    echo $diaMes, '/', $numeroMes, '/', $anio;
+?>
+<hr>
+<?php
+    $fechaYHora = date('d/m/Y H:i:s');
+    echo $fechaYHora;
+?>
+<hr>
+<?php
+    //mostrar día de la semana
+    $diaSemana = date('l');
+    //echo $diaSemana;
+    switch ( $diaSemana )
+    {
+        case 'Monday':
+            $diaSemana = 'Lunes';
+            break;
+        case 'Tuesday':
+            $diaSemana = 'Martes';
+            break;
+        case 'Wednesday':
+            $diaSemana = 'Miércoles';
+            break;
+        case 'Thursday':
+            $diaSemana = 'Jueves';
+            break;
+        case 'Friday':
+            $diaSemana = 'Viernes';
+            break;
+        default:
+            $diaSemana = 'Sábado';
+            break;
+    }
+    echo $diaSemana;
+
