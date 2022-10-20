0bab41a69a4e5a6d9cf2effb3c0e17fa09a33fd4
Marcos <exegeses@gmail.com>
2022-10-20 11:51:48 -0300

    casteo de variables entrecomilladas - NO USAR

---
+++ clase2/casteo.php
@@ -0,0 +1,7 @@
+<?php
+    $nombre = 'marcos';
+    echo 'Tu nombre es: $nombre';//no castea la variable
+    echo '<br>';
+    echo "Tu nombre es: $nombre";//si castea la variable
+    echo '<br>';
+    echo 'Tu nombre es: ', $nombre;
