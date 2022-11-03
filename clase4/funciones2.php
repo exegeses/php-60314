32a19f8766a1ba8a04ae80300152ae5d8902f2af
Marcos <exegeses@gmail.com>
2022-11-03 10:18:16 -0300

    una resolución de tarea función dividir

--- clase4/funciones2.php
+++ clase4/funciones2.php
@@ -1,12 +1,17 @@
 <?php
     //función para dividir dos número
-    function dividir( float $dividendo, float $divisor ) : float
+    function dividir( float $dividendo, float $divisor ) : float | string
     {
-        //TODO chequear que ambos sean números
-        //TODO chequear que el divisor no sea 0
-        //TODO dividir
-        //TODO retornar ?
+        if ( is_numeric( $dividendo ) && is_numeric( $divisor ) ){
+            if( $divisor != 0 ){
+                $resultado = $dividendo / $divisor;
+                return $resultado;
+            }
+            return 'El divisor no puede ser 0';
+        }
+        return 'ambos deben ser números';
     }
+
 
 
     dividir('manzana', 5);
