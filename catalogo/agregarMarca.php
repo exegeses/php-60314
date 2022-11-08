499fb21cc81843879a5d12b5cb13facaccfb014d
Marcos <exegeses@gmail.com>
2022-11-08 10:39:02 -0300

    modificación en reporte de alta correcta

--- catalogo/agregarMarca.php
+++ catalogo/agregarMarca.php
@@ -2,7 +2,13 @@
     //require 'config/config.php';
     require 'funciones/conexion.php';
     require 'funciones/marcas.php';
-    $check = agregarMarca();
+    $check   = agregarMarca();
+    $mensaje = 'No se pudo agregar la marca.';
+    $css     = 'danger';
+    if( $check ){
+        $mensaje = 'Marca agregada correctamente.';
+        $css     = 'success';
+    }
     include 'layout/header.php';
     include 'layout/nav.php';
 ?>
@@ -10,28 +16,12 @@
     <main class="container py-4">
         <h1>Alta de una marca</h1>
 
-<?php
-    if ( $check ){
-?>
-        <div class="alert bg-light p-4 col-8 mx-auto shadow text-success">
-            Marca agregada correctamente.
+        <div class="alert bg-light p-4 col-8 mx-auto shadow text-<?= $css ?>">
+            <?= $mensaje ?>
             <a href="adminMarcas.php" class="btn btn-outline-secondary">
                 Volver a panel de marcas
             </a>
         </div>
-<?php
-    }
-    else{
-?>
-        <div class="alert bg-light p-4 col-8 mx-auto shadow text-danger">
-            No se pudo agregar la marca.
-            <a href="adminMarcas.php" class="btn btn-outline-secondary">
-                Volver a panel de marcas
-            </a>
-        </div>
-<?php
-    }
-?>
 
     </main>
 
